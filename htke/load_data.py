f72c0ebe56f8ab2cf8f385e75f81a8374e6df37c
pdingwall <p.dingwall@qub.ac.uk>
2023-06-01 14:59:14 +0100

    Can read icir7 outputs

--- htke/load_data.py
+++ htke/load_data.py
@@ -13,7 +13,8 @@ class Data:
         
         """
 		Will glob all txt files and return them in a dataframe.
-		This is specifically for IR data"""
+		This is specifically for IR data.
+		"""
         
         globbed_files = glob.glob("*.txt")
 
@@ -38,6 +39,49 @@ class Data:
         ir_data = pd.concat(frame, ignore_index=True) 
         
         return ir_data
+		
+		
+    def read_icir7():
+	
+        """
+        Will glob all txt files and return them in a dataframe.
+        This is for the new format from ICIR 7.1		
+        """
+
+        globbed_files = glob.glob("*.txt")
+
+        frame=[]
+        
+        for txt in globbed_files:
+
+            tmp = pd.read_csv(txt, sep="\t")
+
+            # Add filename as column (make sure that filename.txt is the experiment number)
+            exp_no=os.path.splitext(txt)[0]
+            tmp['Exp No']=exp_no
+
+            # Analysis set up to use the column heading 'Relative Time'
+            tmp.rename(columns={'DateTime':'Relative Time'}, inplace = True)
+
+            # Remove the decimal points for seconds
+            tmp['Relative Time'] = pd.to_datetime(tmp['Relative Time']).apply(lambda x: x.replace(microsecond=0))
+
+            # dt.time removes the date
+            tmp['Relative Time'] = pd.to_datetime(tmp['Relative Time'], format = '%H:%M:%S').dt.time.astype('string')
+
+            # Change hh:mm:ss format into seconds
+            tmp['Relative Time'] = tmp['Relative Time'].str.split(':').apply(lambda x :int(x[0]) * 3600 + int(x[1]) * 60 + int(x[2]))
+
+            # Then back into more useful minutes
+            tmp['Relative Time'] = tmp['Relative Time'].astype(int)/60
+
+            frame.append(tmp)
+
+        ir_data = pd.concat(frame, ignore_index=True) 
+        
+        return ir_data
+		
+		
         
     def plot(ir_data):
         
